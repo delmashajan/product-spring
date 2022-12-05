a92e0d7c1dcaea59f498241a04822b2cdc9da65c
delmashajan <delmashajan@gmail.com>
2022-12-05 17:30:09 +0530

    add product

---
+++ src/main/java/com/nest/productapp_backend/product/ProductController.java
@@ -0,0 +1,13 @@
+package com.nest.productapp_backend.product;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class ProductController {
+    @PostMapping("/")
+    public String Addproduct(){
+        return "add product details";
+    }
+}
